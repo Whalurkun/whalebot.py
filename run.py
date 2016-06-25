b5f3e6b6dd37208f1153835fa8715dbedece9f72
Valen <tomas.valen@ntnu.no>
2016-06-25 16:23:37 -0700

    made it able to join a channel with .play
    Can't leave channel yet.

--- run.py
+++ run.py
@@ -8,12 +8,6 @@ send_message = client.send_message
 @client.event
 async def on_ready():
     
-    #test
-    #users = str(len(set(bot.get_all_members())))
-    #servers = str(len(bot.servers))
-    #channels = str(len([c for c in bot.get_all_channels()]))
-    #test-end
-    
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
@@ -22,17 +16,13 @@ async def on_ready():
     print('Is user bot?')
     print(client.user.bot)
     print('------')
-    #print("Connected to:")
-    #print("{} servers".format(servers))
-    #print("{} channels".format(channels))
-    #print("{} users".format(users))
 
 @client.event
 async def on_message(message):
     
     author = message.author
-
-    if message.content.startswith('!test'):
+    
+    if message.content.startswith('.test'):
         counter = 0
         tmp = await send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -40,26 +30,28 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'Fuck that here is some value {}.'.format(counter))
-    elif message.content.startswith('!sleep'):
+    elif message.content.startswith('.sleep'):
         await asyncio.sleep(5)
         await send_message(message.channel, 'Done sleeping')
 
-    elif message.content.startswith('!play'):
-        await client.join_voice_channel
-
-    elif message.content.startswith('.'):
-        await send_message(message.channel, 'not a command yet.')
-
-    elif message.content.startswith('!hi'):
+    elif message.content.startswith('.play'):
+            await client.join_voice_channel(author.voice_channel)
+            
+    elif message.content.startswith('.hi'):
         await send_message(message.channel, 'Hello {}.'.format(author.mention))
     elif message.content.startswith('hi'):
         await send_message(message.channel, 'Hello {}.'.format(author.mention))
     elif message.content.startswith('hello'):
         await send_message(message.channel, 'Hello {}.'.format(author.mention))
+        
+    elif message.content.startswith('.secret'):
+        await send_message(message.channel, join)
+
+    elif message.content.startswith('.'):
+        await send_message(message.channel, 'not a command yet.')
+
 
 
 
 
 client.run('token')
-
-
